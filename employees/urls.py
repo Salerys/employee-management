d7341292a5fc0c098bf9cc1a92fc03b2ff082059
Salerys <salerysofficial@gmail.com>
2024-09-24 19:48:08 +0200

    added logout function

--- employees/urls.py
+++ employees/urls.py
@@ -1,9 +1,15 @@
 # employee_management/urls.py
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.user_register, name='register'),
+    path(
+        'logout/',
+        LogoutView.as_view(next_page='/login'),
+        name='logout',
+    ),
 ]
