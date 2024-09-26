bc6db92ccfb64f2b58e7cf9000b464fcacdbdd4d
Salerys <salerysofficial@gmail.com>
2024-09-26 09:34:26 +0200

    changed hosts

--- employee_management/settings.py
+++ employee_management/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+ALLOWED_HOSTS = ['https://employee-management-072b60f670f5.herokuapp.com', 'localhost']
 
 
 # Application definition
